871ee8e7dcc3c6bb5aa94c7ad39d497db3a4c80f
Ismadbek <ismadbek@Ismadbeks-MacBook-Air.local>
2025-12-09 17:14:26 +0500

    33

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,15 +1,19 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(private prisma: PrismaService) {
+    constructor(
+        private configService: ConfigService,
+        private prisma: PrismaService
+    ) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: process.env.JWT_SECRET || 'aminov',
+            secretOrKey: configService.get<string>('JWT_SECRET') || 'aminov',
         });
     }
 
